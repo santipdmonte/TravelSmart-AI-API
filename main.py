aab53e7cd3311c49a0970785d99583c2a28be1a6
santipdmonte <santiagopedemonte02@gmail.com>
2025-06-04 08:27:14 -0300

    Add itinerary rendering logic and styles to display travel plans

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from state import ViajeStateInput
 from trip_planner_graph.trip_planner_graph import graph
 import uvicorn
+import json
 
 app = FastAPI()
 
@@ -15,6 +16,10 @@ def home():
 def generar_itinerario(input_state: ViajeStateInput):
     result = graph.invoke(input_state)
     return result
+
+    # with open("itinerario_result.json", encoding="utf-8") as f:
+    #     data = json.load(f)
+    # return data
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
