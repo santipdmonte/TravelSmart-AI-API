38ef20ac9a7ef37fb2489c14031036525c145b0d
SPedemonte <santiagopedemonte02@gmail.com>
2025-09-08 17:11:01 -0300

    fix alembic error

--- database.py
+++ database.py
@@ -13,7 +13,7 @@ DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 
-DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?options=-csearch_path%3Dtravelsmart"
+DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL, echo=False)
 
