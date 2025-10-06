55f5321ef73b7d51fba21fe79cba3b80c461cb8c
SPedemonte <santiagopedemonte02@gmail.com>
2025-10-06 16:41:21 -0300

    Update LLM model from "gemini-2.5-flash" to "gemini-2.5-pro"

--- utils/llm.py
+++ utils/llm.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+    model="gemini-2.5-pro",
     temperature=0.4,
     max_tokens=None,
     timeout=None,
